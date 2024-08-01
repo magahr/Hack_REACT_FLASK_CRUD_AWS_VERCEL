924f0cf5f895021b50aa85f3a918925a072c994f
Adrian Caraballo <carabalapp@gmail.com>
2024-08-01 00:11:28 -0400

    [add] - se agregaron nuevos endpoints utilizando params y query params

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # flask-grupo-8
+
+# Paso 1: activar el virtual env ubicando nuestra carpeta del virtual env creada anteriormente, en mi caso fue "venv"
+    # source venv/Scripts/activate
+
+# Paso 2: instalar las dependencias necesarias las cuales dejare en un archivo requirements.txt
+    # pip install -r requirements.txt
+
+# Paso 3 : Levantar el servidor con el siguiente comando
+    # flask --app hello run
+
+# Paso 4: Probar algunos de los endpoints ya creados, los cuales puedes conseguir yendo directamente al archivo hello.py 
--- hello.py
+++ hello.py
@@ -11,21 +11,25 @@ app = Flask(__name__)
 
 students = [
     {
+        "id": 1,
         "name": 'Adrian',
         "age": 31,
         "major": "Computer Science"
     },
     {
+        "id": 2,
         "name": 'Hiram',
         "age": 23,
         "major": "Computer Science"
     },
     {
+        "id": 3,
         "name": 'Andres',
         "age": 27,
         "major": "Computer Science"
     },
     {
+        "id": 4,
         "name": 'Keishmer',
         "age": 22,
         "major": "Computer Science"
@@ -46,12 +50,43 @@ def social_oplesk():
 def get_students():
     return jsonify(students)
 
-# Ruta para crear un estudiante.
+#  Ruta para obtener un estudiante por params ( Por parametro de ruta )
+@app.route('/students/<int:student_id>', methods=['GET'])
+def get_student_by_id(student_id):
+    for student in students:
+        if student.get('id') == student_id:
+            return jsonify(student)
+    return jsonify({'message': 'El estudiante no ha sido encontrado'})
+    
 
+# Ruta para crear un estudiante.
 @app.route('/create-student', methods=['POST'])
 def create_student():
     data = request.json
     students.append(data)
-    
     return jsonify({'message': 'Estudiante creado correctamente', 'data': data})
-    
+
+# Ruta para borrar todos los estudiantes
+@app.route('/delete-students', methods=['DELETE'])
+def delete_all_students():
+    students.clear()
+    return jsonify({'message': 'Estudiantes borrados correctamente'})
+
+# Ruta para actualizar parcialmente un estudiante
+@app.route('/patch-student', methods=['PATCH'])
+def update_one_student():
+    data = request.json
+    students[0].update(data) # Actualiza el priomer estudiante de la lista.
+    return jsonify({'message': 'Estudiante actualizado parcialmente'})
+
+# Ruta para eliminar un estudiante por query params
+@app.route('/delete-student/', methods=['DELETE'])
+def delete_student_by_name():
+    name = request.args.get('name')
+    if name:
+        for student in students:
+            if student.get('name') == name:
+                students.remove(student)
+                return jsonify({'message': f'Estudiante {name} eliminado'})
+        return jsonify({'message': 'Estudiante no encontrado'})
+    return jsonify({'message': 'Proporciona un nombre de estudiante'})
